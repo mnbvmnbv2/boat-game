e65e1ea5aa3e74e62ad90c9cbf54daec6e058cc4
mnbvmnbv2 <ferdinand@ytteborg.com>
2025-11-03 22:58:16 +0100

    scaling of drops

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import random
+from collections import defaultdict
 from math import sqrt
 
 from dataclasses import dataclass
@@ -20,7 +21,7 @@ FPS = 1000 // FRAME_RATE_UPDATE
 
 G = 360.0
 
-NUM_DROPS = 500
+NUM_DROPS = 2000
 
 
 class Color:
@@ -64,41 +65,48 @@ def drop_update(drops: list[Drop], time_delta: int) -> list[Drop]:
     for drop in drops:
         drop.y_vel -= G * (time_delta / 1000)
 
+    # map drops
+    map_ = defaultdict(list)
+    for drop in drops:
+        x, y = drop.x // SCALE, drop.y // SCALE
+        map_[x, y].append(drop)
+
     # move position
-    for idx, drop in enumerate(drops):
-        new_x = drop.x + drop.x_vel * (time_delta / 1000)
-        new_y = drop.y + drop.y_vel * (time_delta / 1000)
-        for other_idx, other_drop in enumerate(drops):
-            if idx == other_idx:
-                continue
-            center_distance = sqrt(
-                (new_x - other_drop.x) ** 2 + (new_y - other_drop.y) ** 2
-            )
-            if center_distance <= (drop.radius + other_drop.radius):
-                drop.x_vel = -drop.x_vel / 2
-                drop.y_vel = -drop.y_vel / 2
-                break
+    for drops_region in map_.values():
+        for idx, drop in enumerate(drops_region):
+            new_x = drop.x + drop.x_vel * (time_delta / 1000)
+            new_y = drop.y + drop.y_vel * (time_delta / 1000)
+            for other_idx, other_drop in enumerate(drops_region):
+                if idx == other_idx:
+                    continue
+                center_distance = sqrt(
+                    (new_x - other_drop.x) ** 2 + (new_y - other_drop.y) ** 2
+                )
+                if center_distance <= (drop.radius + other_drop.radius):
+                    drop.x_vel = -drop.x_vel / 2
+                    drop.y_vel = -drop.y_vel / 2
+                    break
 
-        out_right = new_x + drop.radius >= WINDOW_SIZE[0]
-        out_left = new_x - drop.radius < 0
-        if out_right or out_left:
-            drop.x_vel = 0
-            drop.x = max(drop.radius, min(WINDOW_SIZE[0] - drop.radius, new_x))
-        out_top = new_y - drop.radius < 0
-        out_bottom = new_y + drop.radius >= WINDOW_SIZE[1]
-        if out_bottom or out_top:
-            drop.y_vel = 0
-            drop.y = max(drop.radius, min(WINDOW_SIZE[1] - drop.radius, new_y))
-        drop.x += drop.x_vel * (time_delta / 1000)
-        drop.y += drop.y_vel * (time_delta / 1000)
+            out_right = new_x + drop.radius >= WINDOW_SIZE[0]
+            out_left = new_x - drop.radius < 0
+            if out_right or out_left:
+                drop.x_vel = 0
+                drop.x = max(drop.radius, min(WINDOW_SIZE[0] - drop.radius, new_x))
+            out_top = new_y - drop.radius < 0
+            out_bottom = new_y + drop.radius >= WINDOW_SIZE[1]
+            if out_bottom or out_top:
+                drop.y_vel = 0
+                drop.y = max(drop.radius, min(WINDOW_SIZE[1] - drop.radius, new_y))
+            drop.x += drop.x_vel * (time_delta / 1000)
+            drop.y += drop.y_vel * (time_delta / 1000)
 
-    return drops
+    return [drop for drops in map_.values() for drop in drops], map_
 
 
 def main():
     mc = View(
         pygame.display.set_mode(WINDOW_SIZE),
-        pygame.Surface((WIDTH, HEIGHT)),
+        pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA),
         pygame.time.Clock(),
         0,
     )
@@ -107,7 +115,7 @@ def main():
         Drop(
             random.randint(0, WINDOW_SIZE[0]),
             random.randint(0, WINDOW_SIZE[1]),
-            random.randint(1, 4),
+            random.randint(1, 2),
             x_vel=random.randint(-100, 100),
         )
         for _ in range(NUM_DROPS)
@@ -129,24 +137,31 @@ def main():
                     pygame.quit()
                     sys.exit()
 
-        mc.game_surface.fill(Color.WHITE)
-
-        draw(mc, gs)
-        gs.drops = drop_update(gs.drops, time_delta)
+        gs.drops, map_ = drop_update(gs.drops, time_delta)
+        draw(mc, gs, map_)
 
         # write fps on screen
         fps = int(mc.clock.get_fps())
         pygame.display.set_caption(f"Boat Game - FPS: {fps}")
 
 
-def draw(mc: View, gs: GameState) -> None:
-    scaled_surface = pygame.transform.scale(mc.game_surface, WINDOW_SIZE)
+def draw(mc: View, gs: GameState, map_: dict) -> None:
+    mc.game_surface.fill((*Color.WHITE, 255))
 
+    max_count = max((len(d) for d in map_.values()), default=1)
+
+    for (cx, cy), drops in map_.items():
+        alpha = int(255 * len(drops) / max_count)
+        color = (*Color.BLUE, alpha)
+        coord = (cx, HEIGHT - cy - 1)
+        mc.game_surface.set_at(coord, color)
+    scaled_surface = pygame.transform.scale(mc.game_surface, WINDOW_SIZE)
     for drop in gs.drops:
         pygame.draw.circle(
-            scaled_surface, Color.BLUE, coord_flip(drop.x, drop.y), drop.radius
+            scaled_surface, (*Color.GREEN, 255), coord_flip(drop.x, drop.y), drop.radius
         )
 
+    mc.screen.fill(Color.WHITE)  # to clear alpha stuff
     mc.screen.blit(scaled_surface, (0, 0))
 
     pygame.display.flip()
