9858f066646b7114b493ad653f35949099437377
mnbvmnbv2 <ferdinand@ytteborg.com>
2025-11-08 15:32:41 +0100

    update random order on collision detect

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ FPS = 1000 // FRAME_RATE_UPDATE
 G = 360.0
 REPULSIVE_FORCE = 1.0
 
-NUM_DROPS = 700
+NUM_DROPS = 500
 
 FIXED_DT = 1.0 / 240.0
 MAX_STEPS_PER_FRAME = 8
@@ -77,6 +77,7 @@ def drop_update(drops: list[Drop], dt: float) -> list[Drop]:
 
     # move position
     for drops_region in map_.values():
+        random.shuffle(drops_region)
         for idx, drop in enumerate(drops_region):
             new_x = drop.x + drop.x_vel * dt
             new_y = drop.y + drop.y_vel * dt
@@ -84,12 +85,12 @@ def drop_update(drops: list[Drop], dt: float) -> list[Drop]:
             out_right = new_x + drop.radius >= WINDOW_SIZE[0]
             out_left = new_x - drop.radius < 0
             if out_right or out_left:
-                drop.x_vel = 0
+                drop.x_vel = -drop.x_vel * 0.05
                 new_x = max(drop.radius, min(WINDOW_SIZE[0] - drop.radius, new_x))
             out_top = new_y - drop.radius < 0
             out_bottom = new_y + drop.radius >= WINDOW_SIZE[1]
             if out_bottom or out_top:
-                drop.y_vel = 0
+                drop.y_vel = -drop.y_vel * 0.05
                 new_y = max(drop.radius, min(WINDOW_SIZE[1] - drop.radius, new_y))
             drop.x = new_x
             drop.y = new_y
@@ -111,7 +112,6 @@ def drop_update(drops: list[Drop], dt: float) -> list[Drop]:
                     drop.y_vel += -y_component * REPULSIVE_FORCE
                     other_drop.x_vel += x_component * REPULSIVE_FORCE
                     other_drop.y_vel += y_component * REPULSIVE_FORCE
-                    break
 
     return [drop for drops in map_.values() for drop in drops], map_
 
@@ -128,7 +128,7 @@ def main():
         Drop(
             random.randint(0, WINDOW_SIZE[0]),
             random.randint(0, WINDOW_SIZE[1]),
-            random.randint(20, 22),
+            random.randint(10, 12),
             x_vel=random.randint(-100, 100),
         )
         for _ in range(NUM_DROPS)
